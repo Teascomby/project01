daae8ef477cbb506eef464d8790fa32f7920b3ca
Teascomby <58056444+Teascomby@users.noreply.github.com>
2019-11-22 07:50:28 -0300

    Create blank.c

---
+++ .github/ fluxos de trabalho/blank.c
@@ -0,0 +1,18 @@
+- nome: Haskell Linter
+  usos: domdere/haskell-lint-action@v1.0.2name: CI
+
+on: [push]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v1
+    - name: Run a one-line script
+      run: echo Hello, world!
+    - name: Run a multi-line script
+      run: |
+        echo Add other actions to build,
+        echo test, and deploy your project.
